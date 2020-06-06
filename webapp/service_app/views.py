0ed2df602f3f506fa4af872e14cbc7127c426c00
Serebrennikov Sergey <Smit-bb@mail.ru>
2020-06-06 21:51:40 +0500

    added service status

--- webapp/service_app/views.py
+++ webapp/service_app/views.py
@@ -1,13 +1,21 @@
-from flask import Blueprint
+import subprocess
+from flask import Blueprint, redirect, url_for, request
 from flask import render_template
 
-blueprint = Blueprint('service', __name__, template_folder='templates',)
+blueprint = Blueprint('service', __name__, template_folder='templates', )
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=['GET', 'POST'])
 def service_apache():
-    title = "service_name = apache2"
-    return render_template('service_app/management_apache.html', title=title)
+    title = "apache2"
+    output = subprocess.check_output(['ps', '-A'])
+    if title in str(output):
+        service_state = True  # приложение запущенно
+    else:
+        service_state = False
+
+    # return redirect(url_for('service.service_apache'))
+    return render_template('service_app/management_apache.html', title=title, service_state=service_state)
 
 
 @blueprint.route('/asterisk')
