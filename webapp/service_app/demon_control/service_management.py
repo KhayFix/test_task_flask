9d74f5a1d4fba9df8a44075fb13628818ce04fb5
Serebrennikov Sergey <Smit-bb@mail.ru>
2020-06-11 15:37:30 +0500

    code refactoring

--- webapp/service_app/demon_control/service_management.py
+++ webapp/service_app/demon_control/service_management.py
@@ -2,20 +2,23 @@
 import os
 from subprocess import Popen, PIPE, TimeoutExpired
 
+from webapp.config import USER_PASSWORD
+
 
 def service_running(command: str, name_app: str) -> int or False:
     """
-    Поместите ваш пароль от пользователя в .bash_profile, или введите его вручную в .communicate('ваш пароль').
+    Функция может управлять приложением Apache2.
+    command='status' возвращает "0" если приложение работает или '3' - не работает.
 
     :param command: status, start, stop, restart.
     :param name_app: Имя приложения, для которого выполняется команда.
-    :return: int: command='status' возвращает "0" если приложение работает или '3' - не работает.
+    :return: int
     """
     try:
         with open(os.devnull, 'wb') as output:
             exit_code = Popen(['sudo', '-S', 'systemctl', command, name_app], stdout=output, stdin=PIPE,
                               stderr=output, universal_newlines=True)
-            exit_code.communicate(os.environ.get("USER_PASSWORD"), timeout=3)  # ввод пароля, если это потребуется.
+            exit_code.communicate(USER_PASSWORD, timeout=3)  # ввод пароля, если это потребуется.
             status_execution = exit_code.wait()
         return status_execution
     except TimeoutExpired:  # создается при истечении времени ожидания в .communicate(timeout=3)
