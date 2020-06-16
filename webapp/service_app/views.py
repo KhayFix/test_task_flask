479605cdb57da288dab0aaebcd9f5c047d8c4cb1
Serebrennikov Sergey <Smit-bb@mail.ru>
2020-06-16 18:04:45 +0500

    the logic was transferred from service_apache2 to the apache2 management template

--- webapp/service_app/views.py
+++ webapp/service_app/views.py
@@ -9,29 +9,28 @@ blueprint = Blueprint('service', __name__, template_folder='templates', )
 
 @blueprint.route('/')
 def service_apache():
+    """
+    service_running возвращает такие значение 0, 3, 4, False.
+
+    0 - сервис работает.
+    3 - сервис не работает
+    4 - сервис не найден в системе.
+    False - ошибка.
+    """
     title = "apache2"
     form = ServiceAppForm()
-
     output = service_running(command='status', name_app=title)
-    if output == 0:
-        service_state = True
-    elif output == 3:
-        service_state = False
-    elif output == 4:
-        service_state = None  # сервис не найден
-    else:
-        service_state = 'error'
 
     return render_template('service_app/management_apache.html', title=title,
-                           service_state=service_state, form=form)
+                           service_state=output, form=form)
 
 
 @blueprint.route('/process-service-apache', methods=['GET', 'POST'])
 def process_service_apache():
     title = 'apache2'
     if request.method == 'POST':
-        buttons_data = request.form.to_dict(flat=False)
-        button_value = [value for value in buttons_data.values()][-1][0]
+        buttons_data = request.form.to_dict()
+        button_value = [value for value in buttons_data.values()][-1]
 
         output = service_running(command=f'{button_value.lower()}', name_app=title)
     return redirect(url_for('service.service_apache'))
