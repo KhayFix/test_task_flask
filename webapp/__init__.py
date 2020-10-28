112b2b6df3d345d065a59dc4cc010da272739163
Serebrennikov Sergey <Smit-bb@mail.ru>
2020-10-28 15:59:21 +0500

    added migrations

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask_migrate import Migrate
 from webapp.config import Configuration
 from webapp.db import db
 from webapp.service_app.views import blueprint as service_app_blueprint
@@ -9,6 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Configuration)
     db.init_app(app)
+    migrate = Migrate(app, db)
     # подключаем блюпринт
     app.register_blueprint(service_app_blueprint)
     return app
