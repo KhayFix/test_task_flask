37070826780e0d528ae0b84ce9a4386015ef699d
Serebrennikov Sergey <Smit-bb@mail.ru>
2020-10-28 15:33:01 +0500

    connected database

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from webapp.config import Configuration
+from webapp.db import db
 from webapp.service_app.views import blueprint as service_app_blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Configuration)
+    db.init_app(app)
     # подключаем блюпринт
     app.register_blueprint(service_app_blueprint)
     return app
--- webapp/service_app/models.py
+++ webapp/service_app/models.py
@@ -1,9 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from webapp.db import db
+from datetime import datetime
 
 
 class CheckboxState(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    press_time = db.Column(db.DateTime, nullable=False)
-    bool_state = None
+    press_time = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    bool_state = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"{self.bool_state} - {self.press_time}"
